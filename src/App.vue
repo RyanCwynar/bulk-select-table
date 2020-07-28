135d6cc6f9cf169b2e0daa0fcf54c19524b3fa5e
Ryan Cwynar <ryan@skulabs.com>
2020-07-28 17:55:38 -0400

    add a card title

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <v-main>
       <v-container>
         <v-card>
+          <v-card-title>
+            Shift + Click Bulk Select Demo
+          </v-card-title>
           <v-data-table
             v-model="selected"
             @current-items="current = $event"
@@ -21,7 +24,7 @@
 </template>
 
 <script>
-import {desserts} from "@/mocks/data.json";
+import { desserts } from "@/mocks/data.json";
 export default {
   name: "App",
   components: {},
@@ -40,35 +43,33 @@ export default {
         { text: "Calories", value: "calories" },
         { text: "Fat (g)", value: "fat" },
       ],
-      desserts: desserts,
+      desserts,
     };
   },
   methods: {
     toggle(isSelected, select) {
       select(!isSelected);
     },
-    clickRow(item, {isSelected, select}) {
-      select(!isSelected)
+    clickRow(item, { isSelected, select }) {
+      select(!isSelected);
     },
-    bulkSelect(e){
-      const { selected, current } = this
-      const shiftOn = e.shiftKey
-      // a perfect storm for bulk selection
-      if(selected.length == 2 && shiftOn){
-        const [a , b] = selected
-        let start = current.findIndex(item => item == a)
-        let end = current.findIndex(item => item == b)
-        if(start - end > 0){
-          let temp = start
-          start = end
-          end = temp
+    bulkSelect({ shiftKey }) {
+      const { selected, current } = this;
+
+      if (selected.length == 2 && shiftKey) {
+        const [a, b] = selected;
+        let start = current.findIndex((item) => item == a);
+        let end = current.findIndex((item) => item == b);
+        if (start - end > 0) {
+          let temp = start;
+          start = end;
+          end = temp;
         }
-        for(let i = start + 1; i < end; i++){
-          selected.push(current[i])
+        for (let i = start + 1; i < end; i++) {
+          selected.push(current[i]);
         }
       }
-    }
-
+    },
   },
 };
 </script>
