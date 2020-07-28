c52f3ca0defc9b9dc923adc82a6e8e5d3fcebc5b
Ryan Cwynar <ryan@skulabs.com>
2020-07-28 17:36:44 -0400

    got it working

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,163 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-card>
+          <v-data-table
+            v-model="selected"
+            @current-items="current = $event"
+            @click.native="bulkSelect"
+            @click:row="clickRow"
+            :headers="headers"
+            :items="desserts"
+            item-key="id"
+            class="elevation-1"
+          ></v-data-table>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  name: "App",
+  components: {},
+  data() {
+    return {
+      current: [],
+      selected: [],
+      headers: [
+        { text: "ID", value: "id" },
+        {
+          text: "Dessert",
+          align: "left",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Calories", value: "calories" },
+        { text: "Fat (g)", value: "fat" },
+      ],
+      desserts: [
+        {
+          id: 1,
+          name: "Frozen Yogurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: "1%",
+        },
+        {
+          id: 2,
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: "1%",
+        },
+        {
+          id: 3,
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: "7%",
+        },
+        {
+          id: 4,
+          name: "Cupcake",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: "8%",
+        },
+        {
+          id: 5,
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: "16%",
+        },
+        {
+          id: 6,
+          name: "Jelly bean",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: "0%",
+        },
+        {
+          id: 7,
+          name: "Lollipop",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: "2%",
+        },
+        {
+          id: 8,
+          name: "Honeycomb",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: "45%",
+        },
+        {
+          id: 9,
+          name: "Donut",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: "22%",
+        },
+        {
+          id: 10,
+          name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: "6%",
+        },
+      ],
+    };
   },
+  methods: {
+    toggle(isSelected, select) {
+      select(!isSelected);
+    },
+    clickRow(item, {isSelected, select}) {
+      select(!isSelected)
+    },
+    bulkSelect(e){
+      const { selected, current } = this
+      const shiftOn = e.shiftKey
+      // a perfect storm for bulk selection
+      if(selected.length == 2 && shiftOn){
+        const [a , b] = selected
+        let start = current.findIndex(item => item == a)
+        let end = current.findIndex(item => item == b)
+        if(start - end > 0){
+          let temp = start
+          start = end
+          end = temp
+        }
+        for(let i = start + 1; i < end; i++){
+          selected.push(current[i])
+        }
+      }
+    }
 
-  data: () => ({
-    //
-  }),
+  },
 };
 </script>
